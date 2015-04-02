ca8bd3b1a767df34da38ad4a7ee4f918babc7531
Danny Blackstock <dannyblackstock@gmail.com>
2015-04-02 00:03:35 -0700

    make the canvas adjust in size to max width and 2500 pixels tall (magic number woo)

--- todo-list-angular/app/scripts/controllers/main.js
+++ todo-list-angular/app/scripts/controllers/main.js
@@ -16,26 +16,41 @@ angular.module('todoListAngularApp')
     ];
 
   // element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
+  window.addEventListener('resize', resizeCanvas, false);
 
   init();
   function init(){
-    console.log("DERPDinosuar");
+    // console.log("DERPDinosuar");
+    // console.log($('#rainBalls').width());
+    // $('#rainBalls').width(window.innerWidth);
+    // $('#rainBalls').height(2500);
+    // resize the canvas to fill browser window dynamically
+
+    resizeCanvas();
     paper.install(window);
     paper.setup('rainBalls');
 
-    
-  var path = new paper.Path();
-  // Give the stroke a color
-  path.strokeColor = 'black';
-  var start = new paper.Point(100, 100);
-  // Move to start and draw a line from there
-  path.moveTo(start);
-  // Note that the plus operator on Point objects does not work
-  // in JavaScript. Instead, we need to call the add() function:
-  path.lineTo(start.add([ 200, -50 ]));
-  // Draw the view now:
-  paper.view.draw();
-
+    var path = new paper.Path();
+    // Give the stroke a color
+    path.strokeColor = 'black';
+    var start = new paper.Point(100, 100);
+    // Move to start and draw a line from there
+    path.moveTo(start);
+    // Note that the plus operator on Point objects does not work
+    // in JavaScript. Instead, we need to call the add() function:
+    path.lineTo(start.add([ 200, -50 ]));
+    // Draw the view now:
+    paper.view.draw();
   }
 
+  function resizeCanvas() {
+    $('#rainBalls').width(window.innerWidth);
+    $('#rainBalls').height(2500);
+
+    /**
+     * Your drawings need to be inside this function otherwise they will be reset when 
+     * you resize the browser window and the canvas goes will be cleared.
+     */
+    // drawStuff(); 
+  }
 });
