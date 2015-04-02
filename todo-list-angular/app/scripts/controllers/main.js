7a37fb47a6949d7b74e72c2f2c7cc898cae0d21f
Danny Blackstock <dannyblackstock@gmail.com>
2015-04-01 18:44:59 -0700

    i can draw a line in a view!

--- todo-list-angular/app/scripts/controllers/main.js
+++ todo-list-angular/app/scripts/controllers/main.js
@@ -14,4 +14,28 @@ angular.module('todoListAngularApp')
       'AngularJS',
       'Karma'
     ];
-  });
+
+  // element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
+
+  init();
+  function init(){
+    console.log("DERPDinosuar");
+    paper.install(window);
+    paper.setup('rainBalls');
+
+    
+  var path = new paper.Path();
+  // Give the stroke a color
+  path.strokeColor = 'black';
+  var start = new paper.Point(100, 100);
+  // Move to start and draw a line from there
+  path.moveTo(start);
+  // Note that the plus operator on Point objects does not work
+  // in JavaScript. Instead, we need to call the add() function:
+  path.lineTo(start.add([ 200, -50 ]));
+  // Draw the view now:
+  paper.view.draw();
+
+  }
+
+});
