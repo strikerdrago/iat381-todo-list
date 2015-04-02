b97905450cf60e258171b7ebee41e76aa0d539f0
Danny Blackstock <dannyblackstock@gmail.com>
2015-04-02 15:45:33 -0700

    A spinning square and the canvas resizes but hammer.js events no work

--- todo-list-angular/app/scripts/controllers/main.js
+++ todo-list-angular/app/scripts/controllers/main.js
@@ -15,42 +15,45 @@ angular.module('todoListAngularApp')
       'Karma'
     ];
 
-  // element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
-  window.addEventListener('resize', resizeCanvas, false);
+    // element.on('mousedown', mouseDown).on('mouseup', mouseUp).on('mousemove', mouseDrag);
+    window.addEventListener('resize', resizeCanvas, false);
+    
+    var hammertime = new Hammer(document.getElementById('rainBalls'));
+    hammertime.on('pan', function(ev) {
+        console.log(ev);
+    });
 
-  init();
-  function init(){
-    // console.log("DERPDinosuar");
-    // console.log($('#rainBalls').width());
-    // $('#rainBalls').width(window.innerWidth);
-    // $('#rainBalls').height(2500);
-    // resize the canvas to fill browser window dynamically
+    init();
+    function init(){
+      // console.log("DERPDinosuar");
+      // console.log($('#rainBalls').width());
+      // $('#rainBalls').width(window.innerWidth);
+      // $('#rainBalls').height(2500);
+      // resize the canvas to fill browser window dynamically
 
-    resizeCanvas();
-    paper.install(window);
-    paper.setup('rainBalls');
+      resizeCanvas();
+      paper.install(window);
+      paper.setup('rainBalls');
 
-    var path = new paper.Path();
-    // Give the stroke a color
-    path.strokeColor = 'black';
-    var start = new paper.Point(100, 100);
-    // Move to start and draw a line from there
-    path.moveTo(start);
-    // Note that the plus operator on Point objects does not work
-    // in JavaScript. Instead, we need to call the add() function:
-    path.lineTo(start.add([ 200, -50 ]));
-    // Draw the view now:
-    paper.view.draw();
-  }
+      var path = new Path.Rectangle([75, 75], [100, 100]);
+      path.strokeColor = 'black';
 
-  function resizeCanvas() {
-    $('#rainBalls').width(window.innerWidth);
-    $('#rainBalls').height(2500);
+      view.onFrame = function(event) {
+        // On each frame, rotate the path by 3 degrees:
+        path.rotate(3);
+      }
 
-    /**
-     * Your drawings need to be inside this function otherwise they will be reset when 
-     * you resize the browser window and the canvas goes will be cleared.
-     */
-    // drawStuff(); 
-  }
+
+    }
+
+    function resizeCanvas() {
+      $('#rainBalls').width(window.innerWidth);
+      $('#rainBalls').height(2500);
+
+      /**
+       * Your drawings need to be inside this function otherwise they will be reset when 
+       * you resize the browser window and the canvas goes will be cleared.
+       */
+      // drawStuff(); 
+    }
 });
