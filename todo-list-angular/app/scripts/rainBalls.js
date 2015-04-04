becc60950505648957b357ac7a77142f46b5eca6
Danny Blackstock <dannyblackstock@gmail.com>
2015-04-03 19:44:42 -0700

    works on index.html

--- todo-list-angular/app/scripts/rainBalls.js
+++ todo-list-angular/app/scripts/rainBalls.js
@@ -4,17 +4,17 @@ var canvas = document.getElementById('rainBalls'),
     context = canvas.getContext('2d');
 
 // resize the canvas to fill browser window dynamically
-window.addEventListener('resize', resizeCanvas, false);
+// window.addEventListener('resize', resizeCanvas, false);
 
-function resizeCanvas() {
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
+// function resizeCanvas() {
+//   canvas.width = window.innerWidth;
+//   canvas.height = window.innerHeight;
 
   /**
    * Your drawings need to be inside this function otherwise they will be reset when 
    * you resize the browser window and the canvas goes will be cleared.
    */
-}
+// }
 
 
 
