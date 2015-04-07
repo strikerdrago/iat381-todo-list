7fad02fb28dfe058be6307543ad0ac591e2178b1
Glen Cheng <strikerdrago@gmail.com>
2015-04-07 03:07:41 -0700

    i take it all back

--- todo-list-angular/dist/scripts/rainBalls.js
+++ todo-list-angular/dist/scripts/rainBalls.js
@@ -794,7 +794,7 @@ tappedTodo = function(){
   // console.log(overlay.style.display);
   // overlay.style.display = "block";
   $( "#overlay" ).toggleClass( "shown" );
-  $( "#textEdit" ).show();
+  $( "#todolist" ).show();
   // console.log("hsl("+balls[ballIndex].path.fillColor.hue+", 100%, 50%)");
   $( "#overlay" ).css("background-color", "hsl("+balls[ballIndex].path.fillColor.hue+", 75%, 50%)");
 }
@@ -876,7 +876,7 @@ textEditSubmit = function() {
   pushBall(balls[ballIndex], ballIndex);
   // overlay.style.display = "none";
   $( "#overlay" ).toggleClass( "shown" );
-  $( "#textEdit" ).hide();
+  $( "#todolist" ).hide();
 }
 
 $("#remind-me-button").on("click", function() {
