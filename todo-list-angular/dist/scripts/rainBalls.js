2543e9c60d314e88db269a22fbbae518b65bceb3
Danny Blackstock <dannyblackstock@gmail.com>
2015-04-07 03:52:35 -0700

    undo undo

--- todo-list-angular/dist/scripts/rainBalls.js
+++ todo-list-angular/dist/scripts/rainBalls.js
@@ -794,7 +794,7 @@ tappedTodo = function(){
   // console.log(overlay.style.display);
   // overlay.style.display = "block";
   $( "#overlay" ).toggleClass( "shown" );
-  $( "#textEdit" ).delay(200).show(0);
+  $( "#textEdit" ).show();
   // console.log("hsl("+balls[ballIndex].path.fillColor.hue+", 100%, 50%)");
   $( "#overlay" ).css("background-color", "hsl("+balls[ballIndex].path.fillColor.hue+", 75%, 50%)");
 }
@@ -876,7 +876,7 @@ textEditSubmit = function() {
   pushBall(balls[ballIndex], ballIndex);
   // overlay.style.display = "none";
   $( "#overlay" ).toggleClass( "shown" );
-  $( "#textEdit" ).delay(200).hide(0);
+  $( "#textEdit" ).hide();
 }
 
 $("#remind-me-button").on("click", function() {
