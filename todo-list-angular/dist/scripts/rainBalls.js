9052724eb438a096e1f194427a5fd72916e285c2
Danny Blackstock <dannyblackstock@gmail.com>
2015-04-07 03:51:24 -0700

    remove the fade

--- todo-list-angular/dist/scripts/rainBalls.js
+++ todo-list-angular/dist/scripts/rainBalls.js
@@ -794,7 +794,7 @@ tappedTodo = function(){
   // console.log(overlay.style.display);
   // overlay.style.display = "block";
   $( "#overlay" ).toggleClass( "shown" );
-  $( "#textEdit" ).delay(200).fadeIn(200);
+  $( "#textEdit" ).delay(200).show(0);
   // console.log("hsl("+balls[ballIndex].path.fillColor.hue+", 100%, 50%)");
   $( "#overlay" ).css("background-color", "hsl("+balls[ballIndex].path.fillColor.hue+", 75%, 50%)");
 }
@@ -876,7 +876,7 @@ textEditSubmit = function() {
   pushBall(balls[ballIndex], ballIndex);
   // overlay.style.display = "none";
   $( "#overlay" ).toggleClass( "shown" );
-  $( "#textEdit" ).delay(200).fadeOut(200);
+  $( "#textEdit" ).delay(200).hide(0);
 }
 
 $("#remind-me-button").on("click", function() {
